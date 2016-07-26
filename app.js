cb7ed7dfb7267826e871280f76581cc0ec8254b9
Eugine Song <Eugine Song>
2016-07-26 23:23:37 +1200

    working date picker using angular - ui / current controller testing filter

--- app.js
+++ app.js
@@ -1,35 +1,45 @@
-var app = angular.module('tdwaApp',[]);
+
+var app = angular.module('tdwaApp',['ui.bootstrap']);
 app.controller('todoController', function todoController($scope) {
-    $scope.sentenceStart = "I need to do ";
-    $scope.sentenceEnd = " until ";
-    $scope.todo = "something";
-    $scope.due = "until the end of my day";
-    $scope.fullSentence = function(){
-        return $scope.sentenceStart + $scope.todo + $scope.sentenceEnd + $scope.due;
+  $scope.sentenceStart = "I need to do ";
+  $scope.sentenceEnd = " until ";
+  $scope.todo = "something";
+  $scope.due = "until the end of my day";
+  $scope.fullSentence = function(){
+    return $scope.sentenceStart + $scope.todo + $scope.sentenceEnd + $scope.due;
+  };
+
+  $scope.tasks = [];
+  var listT = localStorage.getItem("storedTasks");
+
+  if(listT !== null){
+    $scope.tasks = JSON.parse(listT);
+  };
+
+  $scope.addTask = function() {
+    var fullTask = {
+      whatToDo : $scope.todo,
+      due : $scope.due,
+      done : false,
     };
+    $scope.tasks.push(fullTask);
+  };
 
-    $scope.tasks = [];
-    var listT = localStorage.getItem("storedTasks");
+  $scope.removeTask = function(task) {
+    $scope.tasks.splice($scope.tasks.indexOf(task),1);
+  };
 
-    if(listT !== null){
-        $scope.tasks = JSON.parse(listT);
-    };
+  window.onbeforeunload = function() {
+    localStorage.setItem("storedTasks", JSON.stringify($scope.tasks));
+  }
 
-    $scope.addTask = function() {
-        var fullTask = {
-            whatToDo : $scope.todo,
-            due : $scope.due,
-            done : false,
-        };
-        $scope.tasks.push(fullTask);
-    };
+});
 
-    $scope.removeTask = function(task) {
-        $scope.tasks.splice($scope.tasks.indexOf(task),1);
-    };
+app.controller('datePickerController', function ($scope, $filter) {
+  console.log($scope.datePicked);
+  setInterval(intervalDt,1000);
+  function intervalDt() {
 
-    window.onbeforeunload = function() {
-        localStorage.setItem("storedTasks", JSON.stringify($scope.tasks));
-    }
-    //localStorage.setItem("storedTasks", JSON.stringify($scope.tasks));
+    console.log($filter('date')($scope.datePicked,'fullDate'));
+  }
 });
